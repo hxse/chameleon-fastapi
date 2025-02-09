279d6f417d4ffc123f23efb3c1af9083bda19b1a
hxse <narutohy@163.com>
2025-02-09 16:32:58 +0800

    udpate

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-uv run python ./src/plot/bokeh_server.py /app/src/fig_data 30 & uv run python ./src/fast_api_demo.py /app/src/fig_data
+uv run python ./src/plot/bokeh_server.py /app/src/fig_data 15000 & uv run python ./src/fast_api_demo.py /app/src/fig_data
